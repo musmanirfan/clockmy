29f8ec49df505bf96c1a73040e8776fe927c4b15
musmanirfan <usmanmmala356@gmail.com>
2024-07-03 04:56:47 +0500

    latest version

---
+++ script.ts
@@ -0,0 +1,50 @@
+function formatedTime(date: Date): string {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let time = hours >= 12 ? 'PM' : 'AM';
+
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    let minutesStr = minutes < 10 ? '0' + minutes : minutes.toString();
+    let secondsStr = seconds < 10 ? '0' + seconds : seconds.toString();
+    let hoursStr = hours < 10 ? '0' + hours : seconds.toString();
+
+    return `${hoursStr}:${minutesStr}:${secondsStr} ${time}`
+};
+
+function updateClock() {
+    let now = new Date()
+    let formateTime = formatedTime(now);
+    let clockElement = document.querySelectorAll("p#clock");
+    if (clockElement) {
+        clockElement[0].innerHTML = formateTime;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    updateClock();
+    setInterval(updateClock, 1000);
+});
+
+function getCurrentDate(): string {
+    const months: string[] = [
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+    const daysOfWeek: string[] = [
+        'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+    ];
+
+    const now = new Date();
+    const dayOfWeek = daysOfWeek[now.getDay()];
+    const dayOfMonth = now.getDate();
+    const month = months[now.getMonth()];
+    const year = now.getFullYear();
+
+    return `${dayOfWeek} ${dayOfMonth} ${month} ${year}`;
+}
+
+
+const currentDate = getCurrentDate();
+let datePrint = document.querySelectorAll("p")[1].innerHTML = currentDate;
